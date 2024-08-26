d9c22ef0bcac73d7d17d0d594e3136162e527f31
Sunny <sobhit230@gmail.com>
2024-08-26 09:55:02 +0530

    Recursion

--- Part-3/Recursion/sub-1.cpp
+++ Part-3/Recursion/sub-1.cpp
@@ -3,19 +3,22 @@ using namespace std;
 
 //! generate all binary strings without consecutive 1's
 
-void helper(string &s, char prev, int counter, int n, vector<string> &ans) {
-    if(counter == n) {
+void helper(string &s, char prev, int counter, int n, vector<string> &ans)
+{
+    if (counter == n)
+    {
         ans.push_back(s);
         return;
     }
-    
+
     // Append '0' and recurse
     s += '0';
     helper(s, '0', counter + 1, n, ans);
     s.pop_back();
-    
+
     // Append '1' only if the previous character is not '1'
-    if(prev != '1') {
+    if (prev != '1')
+    {
         s += '1';
         helper(s, '1', counter + 1, n, ans);
         s.pop_back();
@@ -24,63 +27,114 @@ void helper(string &s, char prev, int counter, int n, vector<string> &ans) {
 
 //! generate parenthesis
 
-
-void solve(string op, int open , int close, vector<string> &ans){
-    if(open == 0 && close == 0){
+void solve(string op, int open, int close, vector<string> &ans)
+{
+    if (open == 0 && close == 0)
+    {
         ans.push_back(op);
         return;
-    } 
-    if(open == close){
+    }
+    if (open == close)
+    {
         op.push_back('(');
-        
     }
 }
 
-vector<string> generateParenthesis(int n){
+vector<string> generateParenthesis(int n)
+{
     int open = n;
     int close = n;
     vector<string> ans;
     string op = "";
-    solve(op,open,close,ans);
+    solve(op, open, close, ans);
     return ans;
 }
 
-vector<string> generateBinaryStrings(int n) {
+vector<string> generateBinaryStrings(int n)
+{
     vector<string> ans;
     string s = "";
-    helper(s, '0', 0, n, ans);  // Start with an empty string and counter = 0
+    helper(s, '0', 0, n, ans); // Start with an empty string and counter = 0
     return ans;
 }
 
+//! subsets
 
-//! subsets 
-
-void helper1(int counter, vector<int> &nums, vector<int> &temp, vector<vector<int>> &ans){
-    if(counter == nums.size()){
+void helper1(int counter, vector<int> &nums, vector<int> &temp, vector<vector<int>> &ans)
+{
+    if (counter == nums.size())
+    {
         ans.push_back(temp);
         return;
     }
     temp.push_back(nums[counter]);
-    helper1(counter+1,temp,nums,ans);
+    helper1(counter + 1, temp, nums, ans);
     temp.pop_back();
-    helper1(counter+1,temp,nums,ans);
+    helper1(counter + 1, temp, nums, ans);
 }
 
-vector<vector<int>> subsets(vector<int> &nums){
+vector<vector<int>> subsets(vector<int> &nums)
+{
     vector<vector<int>> ans;
     vector<int> temp;
-    helper1(0,nums,temp,ans);
+    helper1(0, nums, temp, ans);
     return ans;
 }
 
+//! find subsets whose sum is k
 
+void printSubsets(int index, vector<int> &ds, int s, int sum, int arr[], int n)
+{
+    if (index == n)
+    {
+        if (s == sum)
+        {
+            for (auto it : ds)
+                cout << it << " ";
+            cout << endl;
+        }
+        return;
+    }
+    ds.push_back(arr[index]);
+    s += arr[index];
+    printSubsets(index + 1, ds, s, sum, arr, n);
+    ds.pop_back();
+    s -= arr[index];
+    printSubsets(index + 1, ds, s, sum, arr, n);
+}
 
+//! Number of subsets with sum k
+
+int MOD = (int)(1e9 + 7);
+void solve(int arr[], int sum, int n, int &ans, int index, int s)
+{
+    if (index == n)
+    {
+        if (s == sum)
+        {
+            ans = (ans + 1) % MOD;
+            // return;
+        }
+        return;
+    }
+    s += arr[index];
+    solve(arr, sum, n, ans, index + 1, s);
+    s -= arr[index];
+    solve(arr, sum, n, ans, index + 1, s);
+}
+int perfectSum(int arr[], int n, int sum)
+{
+    // Your code goes here
+    int ans = 0;
+    solve(arr, sum, n, ans, 0, 0);
+    return ans;
+}
 
 int main()
 {
     int n = 3;
     vector<string> result = generateBinaryStrings(n);
-    for(auto &s : result)
+    for (auto &s : result)
         cout << s << " ";
     return 0;
 }
