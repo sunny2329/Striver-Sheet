6ff79285f09b07e9ea09273684dd16d45d4f7925
Sunny <sobhit230@gmail.com>
2024-09-24 18:25:24 +0530

    1d-DP

--- Part-3/Dynamic Programming/1dDP.cpp
+++ Part-3/Dynamic Programming/1dDP.cpp
@@ -30,6 +30,29 @@ int frogJump(int ind, vector<int> &height, vector<int> &dp)
     return dp[ind] = min(jumpOne, jumpTwo);
 }
 
+int solve(vector<int> &height, int ind, vector<int> &dp)
+{
+    int n = height.size();
+    if (ind == n - 1)
+        return 0;
+    if (dp[ind] != -1)
+        return dp[ind];
+    int oneJump = solve(height, ind + 1, dp) + abs(height[ind] - height[ind + 1]);
+
+    int twoJump = INT_MAX;
+    if (ind + 2 < n)
+    {
+        twoJump = solve(height, ind + 2, dp) + abs(height[ind] - height[ind + 2]);
+    }
+    return dp[ind] = min(oneJump, twoJump);
+}
+int minimumEnergy(vector<int> &height, int n)
+{
+    // Code here
+    vector<int> dp(n + 1, -1);
+    return solve(height, 0, dp);
+}
+
 //! Minimal Cost
 
 int solve(int ind, vector<int> &height, vector<int> &dp, int k)
