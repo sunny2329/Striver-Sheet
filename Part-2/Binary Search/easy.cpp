c8a0ea1339a7be57fe9a04c6238a7e8d8d0ca0ca
Sunny <sobhit230@gmail.com>
2024-09-27 14:26:20 +0530

    Binary Search

--- Part-2/Binary Search/easy.cpp
+++ Part-2/Binary Search/easy.cpp
@@ -241,6 +241,24 @@ bool rotatessearch(vector<int> &nums, int target)
     return false;
 }
 
+int findMin(vector<int> &nums)
+{
+    int s = 0, e = nums.size() - 1;
+    while (s < e)
+    {
+        int mid = s + (e - s) / 2;
+        if (nums[mid] < nums[e])
+        {
+            e = mid;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return nums[s];
+}
+
 int main()
 {
 }
