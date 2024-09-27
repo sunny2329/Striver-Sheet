6e6021b8e8d2151cfaf72d7e3af92c43629a0253
Sunny <sobhit230@gmail.com>
2024-09-27 14:15:59 +0530

    Binary Search

--- Part-2/Binary Search/easy.cpp
+++ Part-2/Binary Search/easy.cpp
@@ -72,6 +72,54 @@ int searchInsert(vector<int> &nums, int target)
     return start;
 }
 
+//! Floor and ceil
+
+int findFloor(int arr[], int n, int x)
+{
+    int low = 0, high = n - 1;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] <= x)
+        {
+            ans = arr[mid];
+            // look for smaller index on the left
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int findCeil(int arr[], int n, int x)
+{
+    int low = 0, high = n - 1;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x)
+        {
+            ans = arr[mid];
+            // look for smaller index on the left
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
 int main()
 {
 }
