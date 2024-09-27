6feb462397c5a522d6c424f2b0d2f90a451a2346
Sunny <sobhit230@gmail.com>
2024-09-27 14:28:19 +0530

    find rotation

--- Part-2/Binary Search/easy.cpp
+++ Part-2/Binary Search/easy.cpp
@@ -241,6 +241,8 @@ bool rotatessearch(vector<int> &nums, int target)
     return false;
 }
 
+//! Find Minimum in Rotated Sorted Array
+
 int findMin(vector<int> &nums)
 {
     int s = 0, e = nums.size() - 1;
@@ -257,6 +259,59 @@ int findMin(vector<int> &nums)
         }
     }
     return nums[s];
+}
+
+//! Find the number of rotations
+
+int findKRotation(vector<int> &arr)
+{
+    int low = 0, high = arr.size() - 1;
+    int ans = INT_MAX;
+    int index = -1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        // search space is already sorted
+        // then arr[low] will always be
+        // the minimum in that search space:
+        if (arr[low] <= arr[high])
+        {
+            if (arr[low] < ans)
+            {
+                index = low;
+                ans = arr[low];
+            }
+            break;
+        }
+
+        // if left part is sorted:
+        if (arr[low] <= arr[mid])
+        {
+            // keep the minimum:
+            if (arr[low] < ans)
+            {
+                index = low;
+                ans = arr[low];
+            }
+
+            // Eliminate left half:
+            low = mid + 1;
+        }
+        else
+        { // if right part is sorted:
+
+            // keep the minimum:
+            if (arr[mid] < ans)
+            {
+                index = mid;
+                ans = arr[mid];
+            }
+
+            // Eliminate right half:
+            high = mid - 1;
+        }
+    }
+    return index;
 }
 
 int main()
