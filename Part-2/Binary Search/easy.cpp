40630548485fe74058f2abbb2b678cc9640ac832
Sunny <sobhit230@gmail.com>
2024-09-27 14:14:30 +0530

    Binary Search

--- Part-2/Binary Search/easy.cpp
+++ Part-2/Binary Search/easy.cpp
@@ -50,7 +50,27 @@ int findFloor(vector<long long> &v, long long n, long long x)
     return ans;
 }
 
+//! Search Insert
 
+int searchInsert(vector<int> &nums, int target)
+{
+    int start = 0, end = nums.size() - 1;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] > target)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return start;
+}
 
 int main()
 {
