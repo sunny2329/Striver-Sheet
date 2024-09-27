15b3552b82f2992d2353f41cb3614cd9f3e6615d
Sunny <sobhit230@gmail.com>
2024-09-27 14:24:43 +0530

    binary search

--- Part-2/Binary Search/easy.cpp
+++ Part-2/Binary Search/easy.cpp
@@ -208,6 +208,39 @@ int rotateSearch(vector<int> &nums, int target)
     return -1;
 }
 
+//! search in rotated array 2
+
+bool rotatessearch(vector<int> &nums, int target)
+{
+    int s = 0, e = nums.size() - 1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+        if (nums[mid] == target)
+            return true;
+        if ((nums[s] == nums[mid]) && (nums[e] == nums[mid]))
+        {
+            s++;
+            e--;
+        }
+        else if (nums[s] <= nums[mid])
+        {
+            if (nums[s] <= target && nums[mid] > target)
+                e = mid - 1;
+            else
+                s = mid + 1;
+        }
+        else
+        {
+            if (nums[mid] < target && nums[e] >= target)
+                s = mid + 1;
+            else
+                e = mid - 1;
+        }
+    }
+    return false;
+}
+
 int main()
 {
 }
