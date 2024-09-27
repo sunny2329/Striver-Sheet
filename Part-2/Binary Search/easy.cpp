8c10a61082f8d8bd63e2ee0f9868e9018981ce7d
Sunny <sobhit230@gmail.com>
2024-09-27 14:32:21 +0530

    Binary Search

--- Part-2/Binary Search/easy.cpp
+++ Part-2/Binary Search/easy.cpp
@@ -314,6 +314,68 @@ int findKRotation(vector<int> &arr)
     return index;
 }
 
+//! Single element in an array
+int singleNonDuplicate(vector<int> &nums)
+{
+    int n = nums.size();
+    if (n == 1)
+        return nums[0];
+    if (nums[0] != nums[1])
+        return nums[0];
+    if (nums[n - 1] != nums[n - 2])
+        return nums[n - 1];
+
+    int s = 1, e = n - 2;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+
+        if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1])
+        {
+            return nums[mid];
+        }
+
+        if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int findPeakElement(vector<int> &nums)
+{
+    int n = nums.size();
+
+    if (n == 1)
+        return 0;
+    if (nums[0] > nums[1])
+        return 0;
+    if (nums[n - 1] > nums[n - 2])
+        return n - 1;
+
+    int s = 1, e = n - 2;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
+
+        if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
+        {
+            return mid;
+        }
+
+        if (nums[mid] > nums[mid - 1])
+            s = mid + 1;
+        else
+            e = mid - 1;
+    }
+    return -1;
+}
+
 int main()
 {
 }
