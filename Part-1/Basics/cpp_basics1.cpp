8c16418368c715182c1417113b2c0687e7e2aeb0
Sobhit Sinha <111562789+sunny2329@users.noreply.github.com>
2025-03-03 10:12:30 +0000

    Basics Started

--- Part-1/Basics/cpp_basics1.cpp
+++ Part-1/Basics/cpp_basics1.cpp
@@ -1,1 +1,12 @@
+#include <bits/stdc++.h>
+#include <iostream> // for input and output operators
+#include <math.h> //for math functions
 
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    cout << "First Work";
+    cout << x;
+}
